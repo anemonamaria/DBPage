56ccdd268acd04bf62442ab96df59abe8b2097c8
Anemona Maria M <57135826+anemonamaria@users.noreply.github.com>
2022-05-11 12:09:59 +0300

    Add files via upload

--- modal.js
+++ modal.js
@@ -2,6 +2,9 @@ let overlay = document.querySelector('.modal-overlay');
 document.getElementById('addArticle').addEventListener('click', function() {
     overlay.classList.add('modal-visible');
 });
+document.getElementById('EDITButton').addEventListener('click', function() {
+    overlay.classList.add('modal-visible');
+});
 document.getElementById('cancel').addEventListener('click', function() {
     overlay.classList.remove('modal-visible')
 });
